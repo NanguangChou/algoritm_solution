8588d87d8fb723c092cd51f6d7d26ef2ed37db79
zhounanguang <zhounanguang123@gmail.com>
2015-12-14 14:21:06 +0800

    solution 3 beats only 13.8%

--- algorithm_003_lengthOfLongestSubstring/algorithm_003_lengthOfLongestSubstring/main.cpp
+++ algorithm_003_lengthOfLongestSubstring/algorithm_003_lengthOfLongestSubstring/main.cpp
@@ -7,9 +7,52 @@
 //
 
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+	int lengthOfLongestSubstring(string s) {
+		int pos = 0;
+		int start = 0;
+		int cur_length = 0;
+		char cur_char  = s[0];
+		string cur_string = "";
+        int max_length=0;
+        int temp =0;
+		if (!cur_char) {
+			return 0;
+		}
+		while (cur_char) {
+
+			if (cur_string.find(cur_char, 0) == string::npos) {
+				cur_string.push_back(cur_char);
+				cur_length++;
+				pos++;
+			}
+			else {
+                temp = cur_string.find(cur_char, 0) + 1;
+				pos = start + temp;
+                start = pos;
+				max_length = max_length > cur_length? max_length:cur_length;
+				cur_length = 0;
+				cur_string = "";
+			}
+			cur_char = s[pos];
+			if (!cur_char) {
+				max_length = max_length > cur_length? max_length:cur_length;
+			}
+		}
+		return max_length;
+	}
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+	// insert code here...
+
+	Solution sol;
+	string str = "cdd";
+	cout << sol.lengthOfLongestSubstring(str) << endl;
+	return 0;
 }
